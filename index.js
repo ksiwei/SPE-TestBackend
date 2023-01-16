90864775bcc6c38d41b077e042879d5d1c7dde93
Siwei Kang <siweikang@gmail.com>
2023-01-16 17:03:19 +0000

    initial setup

---
+++ index.js
@@ -0,0 +1,36 @@
+const server = require("server");
+const { get, post } = server.router;
+const { render, json } = server.reply;
+
+function generateRandomLocale() {
+  var locales = ["FR", "DE", "DK", "CA", "US"];
+
+  var randomNumber = Math.floor(Math.random() * locales.length);
+  return locales[randomNumber];
+}
+
+//Shipping Request/Response format
+let shippingPreference = {
+  option: "YARD",
+  portOption: "",
+};
+
+let serverOptions = {
+  port: 8080,
+  parser: {
+    json: { limit: "1mb" },
+  },
+  security: { csrf: false },
+};
+
+// Launch server with options and a couple of routes
+server(serverOptions, [
+  get("/locale", (ctx) => json({ locale: generateRandomLocale() })),
+  post("/shippingPreference", async (ctx) => {
+    let shippingOption = ctx.data;
+
+    shipping = shippingOption;
+    return "ok";
+  }),
+  get("/shippingPreference", (ctx) => json(shipping)),
+]);
